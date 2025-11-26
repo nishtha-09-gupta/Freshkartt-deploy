e036a339f398de1e912912705785404dc18ca23a
Nishtha Gupta <184469231+nishtha-09-gupta@users.noreply.github.com>
2025-11-26 16:20:15 +0530

    Added Pagination and collapsible orders component

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -1,61 +1,89 @@
-// import React, { useEffect, useState } from 'react'
-// import { useAppContext } from '../../context/AppContext'
+
+// import React, { useEffect, useState } from 'react';
+// import { useAppContext } from '../../context/AppContext';
 // import toast from 'react-hot-toast';
 
 // const SellerLogin = () => {
-//     const {isSeller, setIsSeller, navigate, axios} = useAppContext()
-//     const [email, setEmail] = useState("");
-//     const [password, setPassword] = useState("");
+//   const { isSeller, setIsSeller, navigate, axios } = useAppContext();
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
 
-//     const onSubmitHandler = async (event)=>{
-//         try {
-//             event.preventDefault();
-//             const {data} = await axios.post('/api/seller/login', {email, password})
-//             if(data.success){
-//                 setIsSeller(true)
-//                 navigate('/seller')
-//             }else{
-//                 toast.error(data.message)
-//             }
-//         } catch (error) {
-//             toast.error(error.message)
-//         }
-        
+//   const onSubmitHandler = async (event) => {
+//     event.preventDefault();
+//     try {
+//       const { data } = await axios.post('/api/seller/login', { email, password });
+//       if (data.success) {
+//         setIsSeller(true);
+//         navigate('/seller');
+//       } else {
+//         toast.error(data.message);
+//       }
+//     } catch (error) {
+//       toast.error(error.message);
 //     }
+//   };
 
-//     useEffect(()=>{
-//         if(isSeller){
-//             navigate("/seller")
+  
+//   useEffect(() => {
+//     const checkAuth = async () => {
+//       try {
+//         const { data } = await axios.get('/api/seller/is-auth');
+//         if (data.success) {
+//           setIsSeller(true);
+//           navigate('/seller');
+//         } else {
+//           setIsSeller(false);
 //         }
-//     },[isSeller])
+//       } catch (error) {
+//         setIsSeller(false);
+//       }
+//     };
+//     checkAuth();
+//   }, []);
 
-//   return !isSeller && (
+  
+//   if (isSeller) return null;
+
+//   return (
 //     <form onSubmit={onSubmitHandler} className='min-h-screen flex items-center text-sm text-gray-600'>
-
-//         <div className='flex flex-col gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-w-88 rounded-lg shadow-xl border border-gray-200'>
-//             <p className='text-2xl font-medium m-auto'><span className="text-primary">Seller</span> Login</p>
-//             <p className="text-xs text-gray-500 m-auto">
-//             For testing, use <span className="font-medium">admin@example.com</span> and password <span className="font-medium">12345</span></p>
-//             <div className="w-full ">
-//                 <p>Email</p>
-//                 <input onChange={(e)=>setEmail(e.target.value)} value={email}
-//                  type="email" placeholder="enter you email" 
-//                 className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary" required/>
-//             </div>
-//             <div className="w-full ">
-//                 <p>Password</p>
-//                 <input onChange={(e)=>setPassword(e.target.value)} value={password}
-//                  type="password" placeholder="enter your password"
-//                 className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary" required/>
-//             </div>
-//             <button className="bg-primary text-white w-full py-2 rounded-md cursor-pointer">Login</button>
+//       <div className='flex flex-col gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-w-88 rounded-lg shadow-xl border border-gray-200'>
+//         <p className='text-2xl font-medium m-auto'>
+//           <span className='text-primary'>Seller</span> Login
+//         </p>
+//         <p className='text-xs text-gray-500 m-auto'>
+//           For testing, use <span className='font-medium'>admin@example.com</span> and password{' '}
+//           <span className='font-medium'>12345</span>
+//         </p>
+//         <div className='w-full'>
+//           <p>Email</p>
+//           <input
+//             onChange={(e) => setEmail(e.target.value)}
+//             value={email}
+//             type='email'
+//             placeholder='enter your email'
+//             className='border border-gray-200 rounded w-full p-2 mt-1 outline-primary'
+//             required
+//           />
 //         </div>
-
+//         <div className='w-full'>
+//           <p>Password</p>
+//           <input
+//             onChange={(e) => setPassword(e.target.value)}
+//             value={password}
+//             type='password'
+//             placeholder='enter your password'
+//             className='border border-gray-200 rounded w-full p-2 mt-1 outline-primary'
+//             required
+//           />
+//         </div>
+//         <button className='bg-primary text-white w-full py-2 rounded-md cursor-pointer'>Login</button>
+//       </div>
 //     </form>
-//   )
-// }
+//   );
+// };
 
-// export default SellerLogin
+// export default SellerLogin;
+
 import React, { useEffect, useState } from 'react';
 import { useAppContext } from '../../context/AppContext';
 import toast from 'react-hot-toast';
@@ -80,7 +108,6 @@ const SellerLogin = () => {
     }
   };
 
-  
   useEffect(() => {
     const checkAuth = async () => {
       try {
@@ -98,44 +125,58 @@ const SellerLogin = () => {
     checkAuth();
   }, []);
 
-  
   if (isSeller) return null;
 
   return (
-    <form onSubmit={onSubmitHandler} className='min-h-screen flex items-center text-sm text-gray-600'>
-      <div className='flex flex-col gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-w-88 rounded-lg shadow-xl border border-gray-200'>
-        <p className='text-2xl font-medium m-auto'>
-          <span className='text-primary'>Seller</span> Login
-        </p>
-        <p className='text-xs text-gray-500 m-auto'>
-          For testing, use <span className='font-medium'>admin@example.com</span> and password{' '}
-          <span className='font-medium'>12345</span>
-        </p>
-        <div className='w-full'>
-          <p>Email</p>
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            type='email'
-            placeholder='enter your email'
-            className='border border-gray-200 rounded w-full p-2 mt-1 outline-primary'
-            required
-          />
+    <>
+      <button
+        onClick={() => navigate('/')}
+        className="absolute top-4 left-4 bg-[var(--color-primary)] hover:bg-[var(--color-primary-dull)] text-white px-4 py-2 rounded-md"
+      >
+        ⬅ Back
+      </button>
+
+      <form onSubmit={onSubmitHandler} className='min-h-screen flex items-center text-sm text-gray-600'>
+        <div className='flex flex-col gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-w-88 rounded-lg shadow-xl border border-gray-200'>
+          <p className='text-2xl font-medium m-auto'>
+            <span className='text-primary'>Seller</span> Login
+          </p>
+
+          <p className='text-xs text-gray-500 m-auto'>
+            For testing, use <span className='font-medium'>admin@example.com</span> and password{' '}
+            <span className='font-medium'>12345</span>
+          </p>
+
+          <div className='w-full'>
+            <p>Email</p>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              type='email'
+              placeholder='enter your email'
+              className='border border-gray-200 rounded w-full p-2 mt-1 outline-primary'
+              required
+            />
+          </div>
+
+          <div className='w-full'>
+            <p>Password</p>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              type='password'
+              placeholder='enter your password'
+              className='border border-gray-200 rounded w-full p-2 mt-1 outline-primary'
+              required
+            />
+          </div>
+
+          <button className='bg-primary text-white w-full py-2 rounded-md cursor-pointer'>
+            Login
+          </button>
         </div>
-        <div className='w-full'>
-          <p>Password</p>
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            type='password'
-            placeholder='enter your password'
-            className='border border-gray-200 rounded w-full p-2 mt-1 outline-primary'
-            required
-          />
-        </div>
-        <button className='bg-primary text-white w-full py-2 rounded-md cursor-pointer'>Login</button>
-      </div>
-    </form>
+      </form>
+    </>
   );
 };
 
--- client/src/pages/AllProducts.jsx
+++ client/src/pages/AllProducts.jsx
@@ -3,38 +3,69 @@ import { useAppContext } from '../context/AppContext'
 import ProductCard from '../components/ProductCard'
 
 const AllProducts = () => {
+  const { products, searchQuery } = useAppContext()
+  const [filteredProducts, setFilteredProducts] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const itemsPerPage = 20
 
-    const {products, searchQuery } = useAppContext()
-    const [filteredProducts, setFilteredProducts] = useState([])
+  useEffect(() => {
+    if (searchQuery.length > 0) {
+      setFilteredProducts(
+        products.filter((product) =>
+          product.name.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      )
+    } else {
+      setFilteredProducts(products)
+    }
+    setCurrentPage(1)
+  }, [products, searchQuery])
 
-    useEffect(()=>{
-        if(searchQuery.length > 0){
-            setFilteredProducts(products.filter(
-                product => product.name.toLowerCase().includes(searchQuery.toLowerCase())
-            ))}else{
-                setFilteredProducts(products)
-            }
-    },[products, searchQuery])
+  const inStockProducts = filteredProducts.filter((p) => p.inStock)
+  const totalPages = Math.ceil(inStockProducts.length / itemsPerPage)
+  const startIndex = (currentPage - 1) * itemsPerPage
+  const currentProducts = inStockProducts.slice(startIndex, startIndex + itemsPerPage)
 
   return (
     <div className='mt-16 flex flex-col'>
-    <div className='flex flex-col items-start w-max'>
-      <p className='text-2xl font-medium uppercase text-[#243654] relative inline-block'>
-        All Products
-        <span className='block h-0.5 bg-[var(--color-primary)] rounded-full mt-1 w-full'></span>
-      </p>
-    </div>
-  
-    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6'>
-      {filteredProducts
-        .filter((product) => product.inStock)
-        .map((product, index) => (
+      <div className='flex flex-col items-start w-max'>
+        <p className='text-2xl font-medium uppercase text-[#243654] relative inline-block'>
+          All Products
+          <span className='block h-0.5 bg-[var(--color-primary)] rounded-full mt-1 w-full'></span>
+        </p>
+      </div>
+
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6'>
+        {currentProducts.map((product, index) => (
           <ProductCard key={index} product={product} />
         ))}
-    </div>
-  </div>
-  
+      </div>
 
+      {totalPages > 1 && (
+        <div className='flex items-center justify-center gap-4 mt-6'>
+          <button
+  onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+  disabled={currentPage === 1}
+  className='px-4 py-2 rounded-md bg-[var(--color-primary)] text-white disabled:bg-gray-300'
+>
+  Prev
+</button>
+
+<span className='text-lg font-medium'>
+  {currentPage} / {totalPages}
+</span>
+
+<button
+  onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
+  disabled={currentPage === totalPages}
+  className='px-4 py-2 rounded-md bg-[var(--color-primary)] text-white disabled:bg-gray-300'
+>
+  Next
+</button>
+
+        </div>
+      )}
+    </div>
   )
 }
 
